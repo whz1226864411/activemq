3d3502bf60bbb3e78cb06488cc56565a7cff58d7
whz1226864411 <1226864411@qq.com>
2017-12-24 12:12:47 +0800

    Initial commit

---
+++ src/main/java/com/creat/Producer.java
@@ -0,0 +1,18 @@
+package com.creat;
+
+import org.apache.activemq.ActiveMQConnection;
+
+/**
+ * Created by Administrator on 2017-12-24.
+ */
+public class Producer {
+    //默认用户名
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
+    //默认密码
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+
+    public static void main(String[] args) {
+
+    }
+
+}
