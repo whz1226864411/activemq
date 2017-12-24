2fbaaa93c10a2bc548a895202b889837e9b6d000
whz1226864411 <1226864411@qq.com>
2017-12-24 13:32:22 +0800

    第一次

--- src/main/java/com/creat/Consumer.java
+++ src/main/java/com/creat/Consumer.java
@@ -8,7 +8,7 @@ import javax.jms.*;
 /**
  * Created by Administrator on 2017-12-24.
  */
-public class Producer {
+public class Consumer {
     //默认用户名
     private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
     //默认密码
@@ -19,7 +19,7 @@ public class Producer {
     public static void main(String[] args) {
         //实例化工厂
         ConnectionFactory connectionFactory =
-                new ActiveMQConnectionFactory(Producer.USERNAME, Producer.PASSWORD, Producer.BROKERURL);
+                new ActiveMQConnectionFactory(Consumer.USERNAME, Consumer.PASSWORD, Consumer.BROKERURL);
         Connection connection = null;
         try {
             //创建连接
@@ -30,13 +30,15 @@ public class Producer {
             Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
             //创建一个消息队列
             Destination destination = session.createQueue("hello");
-            //创建生产者
-            MessageProducer messageProducer = session.createProducer(destination);
-            for(int i= 0; i < 10; i++){
-                TextMessage message = session.createTextMessage("ActiveMQ消息:"+i);
-                messageProducer.send(message);
+            MessageConsumer messageConsumer = session.createConsumer(destination);
+            while (true){
+                TextMessage textMessage = (TextMessage) messageConsumer.receive(10000);
+                if (textMessage != null){
+                    System.out.println(textMessage.getText());
+                }else {
+                    break;
+                }
             }
-            session.commit();
         } catch (JMSException e) {
             e.printStackTrace();
         }finally {
@@ -48,7 +50,5 @@ public class Producer {
                 }
             }
         }
-
     }
-
 }
